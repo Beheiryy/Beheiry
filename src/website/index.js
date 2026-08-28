40d284e5abdacd3b26082315588dd5860f237286
Omar Beheiry <omarbeheiry@aucegypt.edu>
2026-08-28 21:21:03 +0300

    initialized some test files and connected index.js to home.html
    even applied some basic CSS and ran into vscode integrated terminal!

--- src/website/index.js
+++ src/website/index.js
@@ -1,1 +1,13 @@
-console.log('hi world');
+const user = {
+    userName : "gg",
+    userPass : ''
+};
+
+function createUser(userName, userPass){
+    console.log(`Username is ${userName} and pass is ${userPass}`)
+}
+
+console.log(user);
+createUser("omar", "1222");
+
+alert("i'm availble!");
