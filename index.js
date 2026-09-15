9933e361f8869662b5632de21309acd274f21723
Omar Beheiry <omarbeheiry@aucegypt.edu>
2026-09-15 12:51:12 +0300

    experimenting with javascript
    figured out how to change on click, how to reset that change, but still haven't figured the corrent things to do. playing around

--- index.js
+++ index.js
@@ -8,3 +8,51 @@ hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
 })
+
+const experiences = document.querySelectorAll('.experience');
+console.log(experiences);
+console.log(typeof(experiences));
+
+// experiences.forEach(ex =>{
+//     ex.addEventListener('click', ()=>{
+//         //ex.style.maxWidth = "50vw";
+//         ex.style.maxHeight = "50vh";
+//         //ex.style.width = "50vw";
+//         ex.style.height = "50vh";
+//         ex.style.transition = ".3s ease";
+//         ex.style.backgroundColor = 'var(--primary-offwhite-transparent)';
+//         ex.style.position= "absolute";
+//         //ex.style.left = "0";
+//         ex.style.zIndex = "10";
+//         ex.style.backdropFilter="blur(5px)";
+//     })
+//     // ex.addEventListener('click', (e) =>{
+//     //     e.style.backgroundColor = 'red';
+//     // })
+//     // ex.style.backgroundColor = 'red';
+// })
+
+const images = document.querySelectorAll('img');
+console.log(images);
+
+// images.forEach((img)=>{
+//     img.clickCount = 0;
+
+//     img.addEventListener('click', ()=>{
+//         if(img.clickCount === 0){
+//             img.clickCount = 1;
+//             img.style.maxWidth = '100vw';
+//             img.style.maxHeight = '100%';
+//             img.style.justifySelf= 'center';
+
+//             img.style.backdropFilter = 'blur(5px)';
+//             img.style.position = 'absolute';
+//             img.style.center = '50vw';
+//         }
+//         else{
+//             img.clickCount = 0;
+//             img.style.backdropFilter = 'blur(0)';
+//             img.style.center='50vw';
+//         }
+//     })
+// })
